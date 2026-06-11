476f880de11e76f202ea3dfd8f2219fbe4a3b90d
sumin <turong92@naver.com>
2026-06-11 16:47:15 +0900

    build: add redis core module

---
+++ modules/redis-core/build.gradle.kts
@@ -0,0 +1,9 @@
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-data-redis")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+    testImplementation("org.assertj:assertj-core")
+    testImplementation("org.springframework.boot:spring-boot-test")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
